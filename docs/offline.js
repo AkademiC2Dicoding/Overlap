7f9bd27c798c3ea62c32a103344adf6db2b275ab
Unknown <punto.damar2@gmail.com>
2019-01-15 00:28:37 +0700

    export

---
+++ docs/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1547486706,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/collision-sheet0.png",
+		"images/overlap-sheet0.png",
+		"images/losoverlap-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
